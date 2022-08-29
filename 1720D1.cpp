0731f8bf5712447edb40ae65205863fddcb25f96
larbert <17835126760@163.com>
2022-08-29 20:44:48 +0800

    1720D1

---
+++ 1720D1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_N = 100005;
+
+int main () {
+    int t, n, a[MAX_N], b[MAX_N];
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << (4 | 2) << endl;
+    }
+    return 0;
+}
